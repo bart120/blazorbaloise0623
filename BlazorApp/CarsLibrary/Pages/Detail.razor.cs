15b1d1de877a5bc376a2ff85167c5f910dd999c8
Vincent Leclerc <vleclerc@inow.fr>
2023-06-05 16:41:45 +0200

    separate code and template

---
+++ BlazorApp/CarsLibrary/Pages/Detail.razor.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components;
+using ServicesLibrary.Models;
+
+namespace CarsLibrary.Pages
+{
+    public partial class Detail:ComponentBase
+    {
+        protected Car? car;
+
+        [Parameter]
+        public int Id { get; set; }
+
+        [Parameter]
+        [SupplyParameterFromQuery(Name = "nom")]
+        public string? Name { get; set; }
+
+        /*public Detail(CarsService servCars, NavigationManager nav)
+        {
+            this.servCars = servCars;
+            this.nav = nav;
+        }*/
+
+        protected override async Task OnInitializedAsync()
+        {
+            this.car = await servCars.GetById(Id);
+        }
+
+        protected async Task Back()
+        {
+            nav.NavigateTo("/cars/list");
+        }
+    }
+}
