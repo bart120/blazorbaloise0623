2f5d31cae70806cd3e50f3e38ab45cd49f34d83d
Vincent Leclerc <vleclerc@inow.fr>
2023-06-05 12:23:47 +0200

    use httpclient & add brands service

--- BlazorApp/BlazorApp/Program.cs
+++ BlazorApp/BlazorApp/Program.cs
@@ -1,11 +1,16 @@
 using BlazorApp;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using ServicesLibrary.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+//builder.Configuration["ApiUrl"]
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["ApiUrl"]) });
+builder.Services.AddScoped<BrandsService>();
 
 await builder.Build().RunAsync();
---
+++ BlazorApp/ServicesLibrary/Services/BrandsService.cs
@@ -0,0 +1,26 @@
+﻿using ServicesLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicesLibrary.Services
+{
+    public class BrandsService
+    {
+        private readonly HttpClient http;
+
+        public BrandsService(HttpClient http)
+        {
+            this.http = http;
+        }
+
+        public async Task<List<Brand>> getAllAsync()
+        {
+            return await http.GetFromJsonAsync<List<Brand>>("brands");
+        }
+
+    }
+}
