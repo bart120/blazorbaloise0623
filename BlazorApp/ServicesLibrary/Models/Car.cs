4f2c878cc025fd41b5510a325cef86c22f55369a
Vincent Leclerc <vleclerc@inow.fr>
2023-06-06 10:30:52 +0200

    edit form & post

--- BlazorApp/ServicesLibrary/Models/Car.cs
+++ BlazorApp/ServicesLibrary/Models/Car.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,13 @@ namespace ServicesLibrary.Models
     public class Car
     {
         public int ID { get; set; }
+        [Required(ErrorMessage = "Modèle obligatoire")]
         public string? Model { get; set; }
-        public int BrandID { get; set; }
+        [Required]
+        public int? BrandID { get; set; }
+        [Required]
         public decimal? Price { get; set; }
+        [Required]
         public DateTime DateOfCirculation { get; set; }
     }
 }
--- BlazorApp/ServicesLibrary/Services/CarsService.cs
+++ BlazorApp/ServicesLibrary/Services/CarsService.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Json;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,14 +18,27 @@ namespace ServicesLibrary.Services
             this.http = http;
         }
 
-        public async Task<List<Car>> GetAllAsync()
+        public async Task<List<Car>?> GetAllAsync()
         {
             return await http.GetFromJsonAsync<List<Car>>("cars");
         }
 
-        public async Task<Car> GetById(int id)
+        public async Task<Car?> GetById(int id)
         {
             return await http.GetFromJsonAsync<Car>($"cars/{id}");
+        }
+
+        public async Task<Car?> AddAsync(Car car)
+        {
+            var resp = await http.PostAsJsonAsync<Car>("cars", car);
+            if (resp.IsSuccessStatusCode)
+            {
+                return await resp.Content.ReadFromJsonAsync<Car>();
+            }
+            else
+            {
+                return null;
+            }
         }
 
         public async Task<Car> DeleteById(int id)
