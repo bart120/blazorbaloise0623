b8852410e8544eb19af98d13db731c13a543ffb0
Vincent Leclerc <vleclerc@inow.fr>
2023-06-13 14:12:15 +0200

    validator

--- BlazorApp/BlazorApp/Models/LoginViewModel.cs
+++ BlazorApp/BlazorApp/Models/LoginViewModel.cs
@@ -1,10 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using BlazorApp.Validators;
+using System.ComponentModel.DataAnnotations;
 
 namespace BlazorApp.Models
 {
     public class LoginViewModel
     {
         [Required]
+        [Email(ErrorMessage = "Format du mail invalide")]
         public string? Email { get; set; }
 
         [Required]
